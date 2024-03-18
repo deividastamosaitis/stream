a84743937b7ff81bd3eafefe4e82265e7fff0fba
deividastamosaitis <tamosaitis.deividas@gmail.com>
2024-03-18 21:07:56 +0200

    gpstiesiogiai

---
+++ src/partials/Header.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../images/logo.png';
+import Dropdown from '../utils/Dropdown';
+
+function Header() {
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
+  const trigger = useRef(null);
+  const mobileNav = useRef(null);
+
+  // close the mobile menu on click outside
+  useEffect(() => {
+    const clickHandler = ({ target }) => {
+      if (!mobileNav.current || !trigger.current) return;
+      if (
+        !mobileNavOpen ||
+        mobileNav.current.contains(target) ||
+        trigger.current.contains(target)
+      )
+        return;
+      setMobileNavOpen(false);
+    };
+    document.addEventListener('click', clickHandler);
+    return () => document.removeEventListener('click', clickHandler);
+  });
+
+  // close the mobile menu if the esc key is pressed
+  useEffect(() => {
+    const keyHandler = ({ keyCode }) => {
+      if (!mobileNavOpen || keyCode !== 27) return;
+      setMobileNavOpen(false);
+    };
+    document.addEventListener('keydown', keyHandler);
+    return () => document.removeEventListener('keydown', keyHandler);
+  });
+
+  return (
+    <header className="absolute w-full z-30">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="flex items-center justify-between h-20">
+          {/* Site branding */}
+          <div className="shrink-0 mr-4">
+            {/* Logo */}
+            <Link to="/" className="block" aria-label="gpsmeistras.lt">
+              <img src={Logo} width={200} alt="gpsmeistras.lt" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
